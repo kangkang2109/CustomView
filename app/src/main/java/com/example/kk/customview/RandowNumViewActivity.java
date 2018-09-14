8a8653b2b31f69cab343790c616e1e105ffd8f05
He Youkang <heyoukang@smartisan.com>
2018-09-14 14:25:05 +0800

    reset

--- app/src/main/java/com/example/kk/customview/RandowNumViewActivity.java
+++ app/src/main/java/com/example/kk/customview/RandowNumViewActivity.java
@@ -13,10 +13,6 @@ import com.example.kk.customview.view.RandowNumView;
 import java.util.ArrayList;
 import java.util.Random;
 
-    import smartisanos.app.MenuDialog;
-    import smartisanos.app.MenuDialogListAdapter;
-    import smartisanos.app.MenuDialogMultiAdapter;
-
 public class RandowNumViewActivity extends AppCompatActivity{
 
 
@@ -30,69 +26,5 @@ public class RandowNumViewActivity extends AppCompatActivity{
             getSupportActionBar().hide();
         }
         mRandowNumView = findViewById(R.id.randow_num);
-        mRandowNumView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Random random = new Random(47);   //种子设置为47，所有设置种子为47的random对象，获得的随机数相同
-//                int randomInt = random.nextInt(9999); //假如每次都创建一个random对象，那么第一次nextInt总是一样
-//                Random random = new Random();
-//                int randomInt = random.nextInt(9999);
-//                mRandowNumView.setTextNum(randomInt);
-                showMenuDialog();
-
-            }
-        });
-        findViewById(R.id.switch_ex).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showMenuMultiDialog();
-            }
-        });
     }
-
-        private void showMenuDialog() {
-            MenuDialog dialog = new MenuDialog(this);
-            dialog.setTitle("TiTle");
-            ArrayList<String> list = new ArrayList<String>();
-            list.add("Apple");
-            list.add("Samsung");
-            list.add("XiaoMi");
-            list.add("Sony");
-
-            ArrayList<View.OnClickListener> listener = new ArrayList<View.OnClickListener>();
-            for (String s : list) {
-                listener.add(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(RandowNumViewActivity.this, "single", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-            MenuDialogListAdapter adapter = new MenuDialogListAdapter(this, list, listener);
-            dialog.setAdapter(adapter);
-
-            dialog.show();
-        }
-
-        private void showMenuMultiDialog() {
-            MenuDialog dialog = new MenuDialog(this);
-            dialog.setTitle("MultiDialog");
-            ArrayList<String> list = new ArrayList<String>();
-            list.add("Apple");
-            list.add("Samsung");
-            list.add("XiaoMi");
-            list.add("Sony");
-            list.add("Smartisan");
-            list.add("Vivo");
-            list.add("360");
-
-            MenuDialogMultiAdapter adapter = new MenuDialogMultiAdapter(this, list);
-            dialog.setAdaper(adapter, new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    Toast.makeText(RandowNumViewActivity.this, "multi", Toast.LENGTH_SHORT).show();
-                }
-            });
-            dialog.show();
-        }
 }
